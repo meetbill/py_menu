d588a2ed9091d1b4955872a94286a5d59d86deb4
meetbill <meetbill@163.com>
2017-10-21 00:40:36 +0800

    update:添加 suse 系统支持

--- tools/install_lib.sh
+++ tools/install_lib.sh
@@ -10,16 +10,16 @@ cd ${CUR_DIR}
 if [[ ! -e "/usr/lib64/libslang.so.2"  ]]
 then
     echo "this system not have libslang.so.2 lib"
-    cp -d ./lib64/libslang.so.2 /usr/lib64/
-    cp ./lib64/libslang.so.2.2.1 /usr/lib64/
+    cp -d ./w_lib64/libslang.so.2 /usr/lib64/
+    cp ./w_lib64/libslang.so.2.2.1 /usr/lib64/
     chmod 777 /usr/lib64/libslang.so.2
 fi
 
 if [[ ! -e "/usr/lib64/libnewt.so.0.52" ]]
 then
     echo "this system not have libnewt.so.0.52"
-    cp -d ./lib64/libnewt.so.0.52 /usr/lib64/
-    cp ./lib64/libnewt.so.0.52.11 /usr/lib64/
+    cp -d ./w_lib64/libnewt.so.0.52 /usr/lib64/
+    cp ./w_lib64/libnewt.so.0.52.11 /usr/lib64/
 fi
 
 
