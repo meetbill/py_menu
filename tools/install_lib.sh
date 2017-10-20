0b0faf94c3046a23f40ff3c53ec1305ddf013c7e
meetbill <meetbill@163.com>
2017-10-21 00:38:42 +0800

    update:添加 suse 系统支持

---
+++ tools/install_lib.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#########################################################################
+# File Name: install_lib.sh
+# Author: meetbill
+# mail: meetbill@163.com
+# Created Time: 2017-10-21 00:18:40
+#########################################################################
+CUR_DIR=$(cd `dirname $0`; pwd)
+cd ${CUR_DIR}
+if [[ ! -e "/usr/lib64/libslang.so.2"  ]]
+then
+    echo "this system not have libslang.so.2 lib"
+    cp -d ./lib64/libslang.so.2 /usr/lib64/
+    cp ./lib64/libslang.so.2.2.1 /usr/lib64/
+    chmod 777 /usr/lib64/libslang.so.2
+fi
+
+if [[ ! -e "/usr/lib64/libnewt.so.0.52" ]]
+then
+    echo "this system not have libnewt.so.0.52"
+    cp -d ./lib64/libnewt.so.0.52 /usr/lib64/
+    cp ./lib64/libnewt.so.0.52.11 /usr/lib64/
+fi
+
+
